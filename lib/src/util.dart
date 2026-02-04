90f7589d76db448bd09be14bbf1eabf8650fd8c1
Kevin Moore <kevmoo@google.com>
2026-02-04 13:49:00 -0800

    A bunch of cleanup fixes

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,5 +1,20 @@
+import 'dart:io' as io;
+
 import 'package:io/ansi.dart';
+import 'package:stack_trace/stack_trace.dart';
 
 void printError(Object? value) {
-  print(wrapWith(value.toString(), [red, styleBold]));
+  io.stderr.writeln(wrapWith(value.toString(), [red, styleBold]));
+}
+
+void setError({
+  required Object message,
+  required int exitCode,
+  StackTrace? stack,
+}) {
+  printError(message);
+  if (stack != null) {
+    io.stderr.writeln(Trace.format(stack));
+  }
+  io.exitCode = exitCode;
 }
